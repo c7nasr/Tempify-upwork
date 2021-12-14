b9efc13902e97780eaee6ffcc9cd8b1acac8e07d
Mahmoud Nasr <42423651+c7nasr@users.noreply.github.com>
2021-12-14 18:10:43 +0200

    refine

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,15 +1,10 @@
-import { checkCookies, getCookie, setCookies } from "cookies-next";
+import React, { useEffect } from "react";
 
+import AuthenticationProtocol from "../api/Auth";
 import Home from "../components/Home";
 import Main from "../components/Main";
-import TempifyAPI from "../api/Connection";
-import { Toaster } from "react-hot-toast";
-import React, { useEffect } from "react";
-import { useRouter } from "next/router";
-import AuthenticationProtocol from "../api/Auth";
-const API = new TempifyAPI();
+
 export default function Index({ user, token, id, password }) {
-  const router = useRouter();
 
   return (
     <Home user={user} token={token} id={id} password={password}>
@@ -19,7 +14,6 @@ export default function Index({ user, token, id, password }) {
 }
 export const getServerSideProps = async ({ req, res }) => {
   const { user, token, id, password } = await AuthenticationProtocol(req, res);
-  console.log(user, token, id, password);
   return {
     props: {
       user,
